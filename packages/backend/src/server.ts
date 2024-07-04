b5878162f6a47a254a78f3e70d590f4cea3d909a
robin marillia <robin4a4@gmail.com>
2024-07-04 22:30:01 +0200

    use vite for the backend

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import express, { type Request, type Response } from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+export const viteNodeApp = app;
