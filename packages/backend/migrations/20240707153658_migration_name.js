96aeccbe957b5725c0cb11d0cee37e0ec55e97f9
robin marillia <robin4a4@gmail.com>
2024-07-07 19:30:16 +0200

    first migration

--- packages/backend/migrations/20240707153658_migration_name.js
+++ packages/backend/migrations/20240707153658_migration_name.js
@@ -2,14 +2,31 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", function (table) {
+      table.increments("id").primary();
+      table.string("username", 255).notNullable();
+      table.string("password", 255).notNullable();
+    })
+    .createTable("boards", function (table) {
+      table.increments("id").primary();
+      table.string("name", 1000).notNullable();
+      table.integer("width").notNullable();
+      table.integer("height").notNullable();
+      table.integer("userId").references("users.id");
+    })
+    .createTable("cells", function (table) {
+      table.increments("id").primary();
+      table.string("name", 1000).notNullable();
+      table.integer("x").notNullable();
+      table.integer("y").notNullable();
+      table.integer("boardId").references("boards.id");
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
