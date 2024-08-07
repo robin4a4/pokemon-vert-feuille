e7c337d39b04e7f6622aa722524881c94e905fe3
robin marillia <robin4a4@gmail.com>
2024-08-07 23:45:23 +0200

    add grid router

---
+++ packages/backend/src/api_router/grid_router.ts
@@ -0,0 +1,88 @@
+import crypto from "crypto";
+import { type NextFunction, type Request, type Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import type { PartialModelObject } from "objection";
+import { type ZodError, z } from "zod";
+import { Grid, User } from "../models";
+import { validate_response } from "./validator";
+
+const grid_router = Router();
+
+const GridBodySchema = z.object({
+	grid: z.string(),
+	username: z.string(),
+});
+
+grid_router
+	.route("/")
+	.get(async (req: Request, res: Response) => {
+		try {
+			const grids = await Grid.query();
+			res.json(validate_response({ status: "success", data: grids }));
+		} catch (e) {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+				validate_response({
+					status: "error",
+					error: (e as any).errors.toString(),
+				}),
+			);
+		}
+	})
+	.post((req: Request, res: Response, next: NextFunction) => {
+		const { username, grid } = GridBodySchema.parse(req.body);
+        Grid.query()
+            .insert({
+                username,
+                grid,
+            } as unknown as PartialModelObject<Grid>)
+            .then((grid) => {
+                res.json(validate_response({ status: "success", data: grid }));
+            })
+            .catch((err) => {
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(validate_response({ status: "error", error: err.toString() }));
+            });
+	});
+
+const GridParamsSchema = z.object({
+    id: z.string(),
+}).transform((params) => {
+    return { id: Number.parseInt(params.id) };
+});
+
+grid_router
+	.route("/:id")
+	.get(async (req: Request, res: Response) => {
+		try {
+			const grid_id = GridParamsSchema.parse(req.params).id;
+			const grid = await Grid.query().findById(grid_id);
+			if (!grid) {
+				res.status(StatusCodes.NOT_FOUND).json(validate_response({ status: "error", error: "Grid not found" }));
+				return;
+			}
+			res.json(validate_response({ status: "success", data: grid }));
+		} catch (e) {
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+				validate_response({
+					status: "error",
+					error: (e as any).errors.toString(),
+				}),
+			);
+		}
+	})
+	.put(async (req: Request, res: Response, next: NextFunction) => {
+        const grid_id = GridParamsSchema.parse(req.params).id;
+		const { username, grid } = GridBodySchema.parse(req.body);
+        Grid.query().findById(grid_id)
+            .patch({
+                username,
+                grid,
+            } as unknown as PartialModelObject<Grid>)
+            .then((grid) => {
+                res.json(validate_response({ status: "success", data: grid }));
+            })
+            .catch((err) => {
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(validate_response({ status: "error", error: err.toString() }));
+            });
+	});
+
+export { grid_router };
--- packages/backend/src/api_router/index.ts
+++ packages/backend/src/api_router/index.ts
@@ -1,12 +1,10 @@
-import { type Request, type Response, Router } from "express";
+import { Router } from "express";
+import { grid_router } from "./grid_router";
 import { user_router } from "./user_router";
 
 const api_router = Router();
 
-api_router.get("/grid", (req: Request, res: Response) => {
-	res.send("grid");
-});
-
+api_router.use("/grids", grid_router)
 api_router.use("/users", user_router);
 
 export { api_router };
